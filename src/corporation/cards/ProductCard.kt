89b0d0c17407c0a42ebc9bab98b3f34a1110a1a9
Denis Shabashov <coutnetnum404@gmail.com>
2025-09-15 02:06:30 +0300

    Override hashCode() in ProductCard.kt

--- src/corporation/cards/ProductCard.kt
+++ src/corporation/cards/ProductCard.kt
@@ -12,4 +12,27 @@ abstract class ProductCard(
         return "$name%$brand%$price"
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ProductCard
+
+        if (price != other.price) return false
+        if (name != other.name) return false
+        if (brand != other.brand) return false
+        if (productType != other.productType) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = price
+        result = 31 * result + name.hashCode()
+        result = 31 * result + brand.hashCode()
+        result = 31 * result + productType.hashCode()
+        return result
+    }
+
+
 }
